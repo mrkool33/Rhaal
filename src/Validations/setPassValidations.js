b58ff979b8f8a95ad919e9d49eff734e04ae8901
SulaimanAlfarsi <132258182+SulaimanAlfarsi@users.noreply.github.com>
2024-12-10 21:18:37 +0400

    Delete src directory

--- src/Validations/setPassValidations.js
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup";
-
-export const setPassValidations=yup.object().shape({
-   
-    vpassword:yup.string().min(6,"Minimum 6 characters").max(15,"Maximum 15 charecters"),
-    vpasswordConfirmation:yup.string().oneOf([yup.ref('vpassword'),null],'Password not match'),
-    
-});
-export default setPassValidations;
