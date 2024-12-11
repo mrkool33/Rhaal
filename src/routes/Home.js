03d16824a5ca0cb43bf92e035f58dcc2f03da00d
mrkool33 <k.for.k44@gmail.com>
2024-12-11 14:29:52 +0400

    Update Home.js
    explore Oman button

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,35 +1,35 @@
-import React from 'react'
-import Navbar from "../components/Navbar"
-import Hero from '../components/Hero'
-import Destiation from '../components/Destiation'
-import Footer from '../components/Footer'
-import image from '../assets/image38.png'
-import StatisticsBanner from '../components/StatisticsBanner'
-import Categories from '../components/Categories'
-
-
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Destiation from "../components/Destiation";
+import Footer from "../components/Footer";
+import image from "../assets/image38.png";
+import StatisticsBanner from "../components/StatisticsBanner";
+import Categories from "../components/Categories";
 
 const Home = () => {
   return (
-  <>
-  <Navbar/>
-  <Hero
-  cName="hero"
-  heroImg={image}
-  title="Embarking on a new adventure"
-  text="Reaching the destination of dreams"
-  buttnText="Travel Plan"
-  url="/"
-  btnClass="show"
-  />
-  <Categories/>
-  <Destiation/>
-  <StatisticsBanner/>
-  <br/><br/><br/>
-  
-  <Footer/>
-  </>
-  )
-}
+    <>
+      <Navbar />
+      <Hero
+        cName="hero"
+        heroImg={image}
+        title="Embarking on a new adventure"
+        text="Reaching the destination of dreams"
+        buttnText="Travel Plan"
+        url="/discover"
+        btnClass="show"
+      />
+      <Categories />
+      <Destiation />
+      <StatisticsBanner />
+      <br />
+      <br />
+      <br />
 
-export default Home
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
