0ba769fd38819b8234eeba02447c4eea79ff548e
mrkool33 <k.for.k44@gmail.com>
2024-12-14 13:41:11 +0400

    booking
    adding booking

---
+++ src/Faetures/bookingSlicer.js
@@ -0,0 +1,97 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initValue = {
+  message: "",
+  booking: [],
+  isLoading: false,
+  isSuccess: false,
+  isError: false,
+};
+
+export const addBookings = createAsyncThunk(
+  "bookings/addBookings",
+  async (bookingData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8080/addBookings",
+        bookingData
+      );
+      return response.data; // Return response data on success
+    } catch (error) {
+      return rejectWithValue(error.response.data); // Handle error properly
+    }
+  }
+);
+export const GetBooking = createAsyncThunk("bookings/GetBooking", async () => {
+  try {
+    const response = await axios.get("http://127.0.0.1:8080/GetBooking");
+    return response.data.Booking; // Return response data on success
+  } catch (error) {
+    return error.response.data; // Handle error properly
+  }
+});
+export const deleteBooking = createAsyncThunk(
+  "bookings/deleteBooking",
+  async (BookingID) => {
+    try {
+      const response = await axios.delete(
+        `http://127.0.0.1:8080/deleteBooking/${BookingID}`
+      );
+      return response.data.message; // Return response data of user only
+    } catch (error) {
+      return error.response.data; // Handle error properly
+    }
+  }
+);
+// Create slice
+const BookingSlice = createSlice({
+  name: "bookings",
+  initialState: initValue,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(addBookings.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(addBookings.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.message = action.payload;
+      })
+      .addCase(addBookings.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload; // Capture error message
+      })
+      .addCase(GetBooking.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(GetBooking.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.booking = action.payload;
+      })
+      .addCase(GetBooking.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload; // Capture error message
+      })
+      .addCase(deleteBooking.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteBooking.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.message = "deleted...";
+      })
+      .addCase(deleteBooking.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload; // Capture error message
+      });
+  },
+});
+
+// Export the reducer
+export default BookingSlice.reducer;
--- src/Store/Store.js
+++ src/Store/Store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import UserReducer from "../Faetures/UserSlicer";
 import LocationsReducer from "../Faetures/locationSlicer";
 import ItemReducer from "../Faetures/itemsSlicer";
+import BookingReducer from "../Faetures/bookingSlicer";
 export const store = configureStore({
   reducer: {
     usersInfo: UserReducer,
     locations: LocationsReducer,
     items: ItemReducer,
+    Bookings: BookingReducer,
   },
 });
