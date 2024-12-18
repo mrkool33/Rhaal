c8023afacdb0b0bcacab6926ee410697b7661337
SulaimanAlfarsi <132258182+SulaimanAlfarsi@users.noreply.github.com>
2024-12-18 19:15:45 +0400

    update in footer desgin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,64 +1,45 @@
-import React from 'react'
-import logo from '../assets/logo.png'
+import React from 'react';
+import logo from '../assets/logo.png';
+import './Footer.css';
 
-import "./Footer.css"
 const Footer = () => {
   return (
     <div className='footer'>
-        <div className='top'>
-            <div>
-            <img src={logo} className="footer-logo" alt="Logo" />  
-            </div>
-            <div>
-            <a href='/'>
-                <i className='fa-brands fa-facebook-square'></i>
-            </a>
-            <a href='/'>
-                <i className="fa-brands fa-twitter"></i>
-            </a>
-            <a href='/'>
-                <i className='fa-brands fa-instagram'></i>
-            </a>
+      {/* Top Section */}
+      <div className='footer-top'>
+        <img src={logo} className='footer-logo' alt='Logo' />
+      </div>
 
-            </div>
+      {/* Center Links */}
+      <div className='footer-links'>
+        <a href='/about'>About</a>
+        <a href='/contact'>Contact</a>
+        <a href='/discover'>Discover</a>
+      </div>
+
+      {/* Bottom Section */}
+      <div className='footer-bottom'>
+        <div className='footer-icons'>
+          <a href='/'><i className='fa-brands fa-facebook'></i></a>
+          <a href='/'><i className='fa-brands fa-twitter'></i></a>
+          <a href='/'><i className='fa-brands fa-telegram'></i></a>
+          <a href='/'><i className='fa-brands fa-instagram'></i></a>
         </div>
-       
-
-        <div className='bottom'>
-            <div>
-                <h4>Projects</h4>
-                <a href='/'>Changelog</a>
-                <a href='/'>Status</a>
-                <a href='/'>License</a>
-                <a href='/'>All Versions</a>
-            </div>
-
-            <div>
-                <h4>Community</h4>
-                <a href='/'>GitHub</a>
-                <a href='/'>Issues</a>
-                <a href='/'>Projects</a>
-                <a href='/'>Twitter</a>
-            </div>
-
-            <div>
-                <h4>Help</h4>
-                <a href='/'>Support</a>
-                <a href='/'>Troupleshooting</a>
-                <a href='/'>Contact Us</a>
-                
-            </div>
-
-            <div>
-                <h4>Others</h4>
-                <a href='/'>Terms of Service</a>
-                <a href='/'>Privacy Policy</a>
-                <a href='/'>License</a>
-            </div>
+        <div className='footer-legal'>
+          <a href='#!'>Privacy Policy</a>
+          <a href='#!'>Terms of Use</a>
+          <a href='#!'>Sales and Refunds</a>
+          <a href='#!'>Legal</a>
+          <a href='#!'>Site Map</a>
         </div>
+      </div>
+
+      {/* Copyright */}
+      <div className='footer-copyright'>
+        <p>&copy; Rahaal 2024 All Rights Reserved</p>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
