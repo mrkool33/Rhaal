d4ff3b3195a8782e741075284a044c0711445552
mrkool33 <k.for.k44@gmail.com>
2024-12-20 11:08:40 +0400

    links change from a to Link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,49 @@
-import React from 'react';
-import logo from '../assets/logo.png';
-import './Footer.css';
-
+import React from "react";
+import logo from "../assets/logo.png";
+import "./Footer.css";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div className='footer'>
+    <div className="footer">
       {/* Top Section */}
-      <div className='footer-top'>
-        <img src={logo} className='footer-logo' alt='Logo' />
+      <div className="footer-top">
+        <img src={logo} className="footer-logo" alt="Logo" />
       </div>
 
       {/* Center Links */}
-      <div className='footer-links'>
-        <a href='/about'>About</a>
-        <a href='/contact'>Contact</a>
-        <a href='/discover'>Discover</a>
+      <div className="footer-links">
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/discover">Discover</Link>
       </div>
 
       {/* Bottom Section */}
-      <div className='footer-bottom'>
-        <div className='footer-icons'>
-          <a href='/'><i className='fa-brands fa-facebook'></i></a>
-          <a href='/'><i className='fa-brands fa-twitter'></i></a>
-          <a href='/'><i className='fa-brands fa-telegram'></i></a>
-          <a href='/'><i className='fa-brands fa-instagram'></i></a>
+      <div className="footer-bottom">
+        <div className="footer-icons">
+          <a href="/">
+            <i className="fa-brands fa-facebook"></i>
+          </a>
+          <a href="/">
+            <i className="fa-brands fa-twitter"></i>
+          </a>
+          <a href="/">
+            <i className="fa-brands fa-telegram"></i>
+          </a>
+          <a href="/">
+            <i className="fa-brands fa-instagram"></i>
+          </a>
         </div>
-        <div className='footer-legal'>
-          <a href='#!'>Privacy Policy</a>
-          <a href='#!'>Terms of Use</a>
-          <a href='#!'>Sales and Refunds</a>
-          <a href='#!'>Legal</a>
-          <a href='#!'>Site Map</a>
+        <div className="footer-legal">
+          <a href="#!">Privacy Policy</a>
+          <a href="#!">Terms of Use</a>
+          <a href="#!">Sales and Refunds</a>
+          <a href="#!">Legal</a>
+          <a href="#!">Site Map</a>
         </div>
       </div>
 
       {/* Copyright */}
-      <div className='footer-copyright'>
+      <div className="footer-copyright">
         <p>&copy; Rahaal 2024 All Rights Reserved</p>
       </div>
     </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from "react";
 import "./Hero.css";
-
+import { Link } from "react-router-dom";
 const Hero = (props) => {
   return (
     <>
-    <div className={props.cName}>
-        <img alt='HerpImg' src={props.heroImg}/>
+      <div className={props.cName}>
+        <img alt="HerpImg" src={props.heroImg} />
 
-        <div className='hero-text'>
-        <h1>{props.title}</h1>
-        <p>{props.text}</p>
-        <a href={props.url} className={props.btnClass}>
+        <div className="hero-text">
+          <h1>{props.title}</h1>
+          <p>{props.text}</p>
+          <Link to={props.url} className={props.btnClass}>
             {props.buttonText}
             explore Oman
-        </a>
+          </Link>
         </div>
-     </div>
-     </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
