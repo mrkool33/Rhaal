f69094a8e2c20a3a89f2a4c3b332cfbf0a56b36d
mrkool33 <k.for.k44@gmail.com>
2024-12-21 15:10:41 +0400

    validations

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import { Container, Row, Col, Form, FormGroup } from "react-bootstrap";
+import { Container, Row, Col, Form, FormGroup, Button } from "react-bootstrap";
 import illustration from "../assets/illustration2.png"; // Replace with your illustration path
 import rectangleImage from "../assets/Rectangle.png"; // Replace with your rectangle image path
 import logo from "../assets/logo.png"; // Replace with your logo path
 import "./Login.css";
 import { Label } from "reactstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { LoginValidations } from "../Validations/LoginValidations.js";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -13,35 +13,38 @@ import { useDispatch, useSelector } from "react-redux";
 import { GetUser } from "../Faetures/UserSlicer.js";
 import { useState } from "react";
 import { FaArrowAltCircleLeft } from "react-icons/fa";
-
 const SignIn = () => {
-  let [email, setEmail] = useState("");
-  let [password, setPassword] = useState("");
-  let navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const dispatch = useDispatch();
-  const { user, isSuccess, isError, message } = useSelector(
+  const navigate = useNavigate();
+
+  const { isSuccess, isError, message } = useSelector(
     (state) => state.usersInfo
   );
 
   const {
     register,
-    handleSubmit: submitForm,
+    handleSubmit,
     formState: { errors },
-  } = useForm({ resolver: yupResolver(LoginValidations) });
+  } = useForm({
+    resolver: yupResolver(LoginValidations),
+  });
 
-  const handleSubmit = () => {
-    const user = { email: email, password: password };
-    dispatch(GetUser(user));
+  const onSubmit = (data) => {
+    dispatch(GetUser(data))
+      .unwrap()
+      .catch((errorMessage) => {
+        console.error("Login failed:", errorMessage);
+      });
   };
+
   useEffect(() => {
-    if (user && isSuccess && message === "login") {
+    if (isSuccess && message === "login") {
       navigate("/");
     }
-    if (isError) {
-      navigate("/login");
-    }
-  }, [isSuccess, isError, user, message, navigate]);
-
+  }, [isSuccess, message, navigate]);
   const handleBack = () => {
     navigate("/");
   };
@@ -61,79 +64,67 @@ const SignIn = () => {
       >
         <FaArrowAltCircleLeft />
       </div>
-
       <Container fluid className="signin-page">
         <Row className="h-100">
-          {/* Left section - Form */}
           <Col
             md={6}
             className="left-section d-flex flex-column align-items-center justify-content-center"
           >
             <img src={logo} alt="Logo" className="logo" />
             <h2 className="welcome-title">Welcome Back!!</h2>
-            <Form className="form-container">
+            <Form className="form-container" onSubmit={handleSubmit(onSubmit)}>
               <Form.Group controlId="formEmail" className="mb-4">
-                <Form.Label className="form-label-left">Email</Form.Label>
-                <div className="input-icon">
-                  <i className="fas fa-envelope"></i>
-                  <Form.Control
-                    type="email"
-                    placeholder="email@gmail.com"
-                    className="inputBox"
-                    {...register("email", {
-                      // value:{email},
-                      onChange: (e) => setEmail(e.target.value),
-                    })}
-                    value={email}
-                  />
-                </div>
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter your email"
+                  className={errors.email ? "is-invalid" : ""}
+                  {...register("email")}
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
+                <div className="invalid-feedback">{errors.email?.message}</div>
               </Form.Group>
               <Form.Group controlId="formPassword" className="mb-4">
-                <Form.Label className="form-label-left">Password</Form.Label>
-                <div className="input-icon">
-                  <i className="fas fa-lock"></i>
-                  <Form.Control
-                    type="password"
-                    placeholder="Enter your password"
-                    className="inputBox"
-                    {...register("password", {
-                      // value:{password},
-                      onChange: (e) => setPassword(e.target.value),
-                    })}
-                    value={password}
-                  />
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter your password"
+                  className={errors.password ? "is-invalid" : ""}
+                  {...register("password")}
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                />
+                <div className="invalid-feedback">
+                  {errors.password?.message}
                 </div>
               </Form.Group>
-              <Link to="/forgot-password" className="forgot-password">
+              <a href="/forgot-password" className="forgot-password">
                 Forgot Password?
-              </Link>
-              <button
-                className="signin-button"
-                onClick={submitForm(handleSubmit)}
-              >
+              </a>
+              <Button type="submit" className="signin-button">
                 Login
-              </button>
+              </Button>
+              {isError && <p className="error">Error: {message}</p>}
               <FormGroup className="signup-checkbox-group">
                 <Label check className="signup-checkbox-label">
                   <span className="signup-text">
-                    Don’t have an account?{" "}
-                    <Link to="/signup" className="signup-link">
+                    Don't have an account?{" "}
+                    <a href="/signup" className="signup-link">
                       Sign up now
-                    </Link>
+                    </a>
                   </span>
                 </Label>
               </FormGroup>
             </Form>
           </Col>
-
-          {/* Right section - Background Image with Illustration */}
           <Col
             md={6}
             className="right-section d-flex align-items-center justify-content-center"
           >
             <img
               src={rectangleImage}
-              alt="Background Rectangle"
+              alt="Background"
               className="background-image"
             />
             <div className="illustration-container">
